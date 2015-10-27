43a78a24a8ed89af17ecc51a6e8b16681fbfd3e0
SeyDutch <ehfransen@live.nl>
2015-10-27 22:37:07 +0100

    PgwSlideshow for gallery. Styles and layout for theme.

---
+++ src/Orchard.Web/Themes/AirbrushTheme/Providers/Layouts/CarouselLayout.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Orchard.ContentManagement;
+using Orchard.DisplayManagement;
+using Orchard.Localization;
+using Orchard.Projections.Descriptors.Layout;
+using Orchard.Projections.Models;
+using Orchard.Projections.Services;
+
+namespace AirbrushTheme.Providers.Layouts
+{
+    //http://www.stevetaylor.me.uk/image-carousel-using-twitter-bootstrap-and-orchard-cms-projections
+    public class CarouselLayout : ILayoutProvider
+    {
+        private readonly IContentManager _contentManager;
+        protected dynamic Shape { get; set; }
+
+        public CarouselLayout(IShapeFactory shapeFactory, IContentManager contentManager)
+        {
+            _contentManager = contentManager;
+            Shape = shapeFactory;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public void Describe(DescribeLayoutContext describe)
+        {
+            describe.For("Html", T("Html"), T("Html Layouts"))
+                .Element("Carousel", T("Carousel"), T("Organizes content items in a carousel."),
+                    DisplayLayout,
+                    RenderLayout,
+                    "CarouselLayout"
+                );
+        }
+
+        public LocalizedString DisplayLayout(LayoutContext context)
+        {
+            string columns = context.State.Columns;
+            bool horizontal = Convert.ToString(context.State.Alignment) != "vertical";
+
+            return horizontal
+                       ? T("{0} columns grid", columns)
+                       : T("{0} lines grid", columns);
+        }
+
+        public dynamic RenderLayout(LayoutContext context, IEnumerable<LayoutComponentResult> layoutComponentResults)
+        {
+
+            string outerDivClass = context.State.OuterDivClass;
+            string outerDivId = context.State.OuterDivId;
+            string innerDivClass = context.State.InnerDivClass;
+            string firstItemClass = context.State.FirstItemClass;
+            string itemClass = context.State.ItemClass;
+
+            IEnumerable<dynamic> shapes =
+               context.LayoutRecord.Display == (int)LayoutRecord.Displays.Content
+                   ? layoutComponentResults.Select(x => _contentManager.BuildDisplay(x.ContentItem, context.LayoutRecord.DisplayType))
+                   : layoutComponentResults.Select(x => x.Properties);
+
+            return Shape.Carousel(Id: outerDivId, Items: shapes, OuterClasses: new[] { outerDivClass }, InnerClasses: new[] { innerDivClass }, FirstItemClasses: new[] { firstItemClass }, ItemClasses: new[] { itemClass });
+        }
+    }
+}
---
+++ src/Orchard.Web/Themes/AirbrushTheme/Providers/Layouts/LayoutShapes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using Orchard.DisplayManagement;
+using Orchard.Localization;
+using Orchard;
+
+namespace AirbrushTheme.Providers.Layouts
+{
+    public class LayoutShapes : IDependency
+    {
+        public LayoutShapes()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        [Shape]
+        public void Carousel(dynamic Display, TextWriter Output, HtmlHelper Html, string Id, IEnumerable<dynamic> Items, IEnumerable<string> OuterClasses, IDictionary<string, string> OuterAttributes, IEnumerable<string> InnerClasses, IDictionary<string, string> InnerAttributes, IEnumerable<string> FirstItemClasses, IDictionary<string, string> FirstItemAttributes, IEnumerable<string> ItemClasses, IDictionary<string, string> ItemAttributes)
+        {
+            if (Items == null)
+                return;
+
+            var items = Items.ToList();
+            var itemsCount = items.Count;
+
+            if (itemsCount < 1)
+                return;
+
+            var outerDivTag = GetTagBuilder("div", Id, OuterClasses, OuterAttributes);
+            var innerDivTag = GetTagBuilder("div", string.Empty, InnerClasses, InnerAttributes);
+            var firstItemTag = GetTagBuilder("div", string.Empty, FirstItemClasses, FirstItemAttributes);
+            var itemTag = GetTagBuilder("div", string.Empty, ItemClasses, ItemAttributes);
+           
+            Output.Write(outerDivTag.ToString(TagRenderMode.StartTag));
+            Output.Write(outerDivTag.ToString(TagRenderMode.StartTag));
+
+            var firstItem = true;
+            foreach (var item in items)
+            {
+                if (firstItem)
+                    Output.Write(firstItemTag.ToString(TagRenderMode.StartTag));
+                else
+                    Output.Write(itemTag.ToString(TagRenderMode.StartTag));
+
+                Output.Write(Display(item));
+                //firstItemTag is also a div
+                Output.Write(itemTag.ToString(TagRenderMode.EndTag));
+                firstItem = false;
+            }
+
+            Output.Write(innerDivTag.ToString(TagRenderMode.EndTag));
+
+            Output.Write("<a href=\"#{0}\" class=\"carousel-control left\" data-slide=\"prev\">&lsaquo;</a>", Id);
+            Output.Write("<a href=\"#{0}\" class=\"carousel-control right\" data-slide=\"next\">&lsaquo;</a>", Id);
+
+            Output.Write(outerDivTag.ToString(TagRenderMode.EndTag));
+
+            Output.Write("<div id='carouselIdRef' style=\"display:none;\" data-carousel-ref=\"" + Id + "\"></div>");
+        }
+
+        static TagBuilder GetTagBuilder(string tagName, string id, IEnumerable<string> classes, IDictionary<string, string> attributes)
+        {
+            var tagBuilder = new TagBuilder(tagName);
+            tagBuilder.MergeAttributes(attributes, false);
+            foreach (var cssClass in classes ?? Enumerable.Empty<string>())
+                tagBuilder.AddCssClass(cssClass);
+            if (!string.IsNullOrWhiteSpace(id))
+                tagBuilder.GenerateId(id);
+            return tagBuilder;
+        }
+    }
+}
